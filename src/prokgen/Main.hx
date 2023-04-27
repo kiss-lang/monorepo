cba27506a6babcd07749e72245fdd1d328084026
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-27 10:04:34 -0600

    Squashed 'kiss-libraries/prokgen/' content from commit a27f42bf
    git-subtree-dir: kiss-libraries/prokgen
    git-subtree-split: a27f42bf9dc7564c0d52437d1c0c474e7104d444

---
+++ src/prokgen/Main.hx
@@ -0,0 +1,10 @@
+package prokgen;
+
+import kiss.Kiss;
+import kiss.Prelude;
+import prokgen.ProkRandom;
+import prokgen.generators.*;
+import prokgen.examples.*;
+
+@:build(kiss.Kiss.build())
+class Main {}
