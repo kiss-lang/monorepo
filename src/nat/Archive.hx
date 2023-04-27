d5cfdea587f3ca28208852b61fa23bba240d3c97
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-27 09:27:27 -0600

    Squashed 'libraries/nat-archive-tool/' content from commit 49286a88
    git-subtree-dir: libraries/nat-archive-tool
    git-subtree-split: 49286a88cdb99cca02dde29bcb94edcc0724f386

---
+++ src/nat/Archive.hx
@@ -0,0 +1,13 @@
+package nat;
+
+import sys.FileSystem;
+import kiss.Prelude;
+import sys.io.File;
+import uuid.Uuid;
+import haxe.DynamicAccess;
+import haxe.ds.Option;
+
+using haxe.io.Path;
+
+@:build(kiss.Kiss.build())
+class Archive {}
